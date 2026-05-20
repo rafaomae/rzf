2fb2dde7e9f82488e68628e3880b3defb04ced00
rafaomae <rafael.omae@gmail.com>
2026-05-20 01:28:26 -0300

    test(walk): improve some unit tests in walk command

--- src/walk.rs
+++ src/walk.rs
@@ -29,21 +29,63 @@ pub fn walk_dir(root: &Path) -> io::Result<Vec<String>> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::path::Path;
+    use std::path::{Path, PathBuf};
+
+    fn create_temp_dir(name: &str) -> PathBuf {
+        let dir = std::env::temp_dir().join(format!("walk-{name}"));
+        std::fs::create_dir_all(&dir).unwrap();
+        dir
+    }
 
     #[test]
     fn walks_current_dir() {
-        let result = walk_dir(Path::new(".")).unwrap();
-        assert!(result.iter().any(|p| p.ends_with("Cargo.toml")));
-        assert!(result.iter().any(|p| p.contains("main.rs")));
+        let dir = create_temp_dir("current");
+
+        std::fs::write(dir.join("a.txt"), "").unwrap();
+        std::fs::write(dir.join("b.txt"), "").unwrap();
+        std::fs::create_dir_all(dir.join("test")).unwrap();
+        std::fs::write(dir.join("test/c.txt"), "").unwrap();
+
+        let result = walk_dir(&dir).unwrap();
+        assert!(result.iter().any(|p| p.ends_with("a.txt")));
+        assert!(result.iter().any(|p| p.ends_with("b.txt")));
+        assert!(result.iter().any(|p| p.ends_with("test/c.txt")));
+
+        std::fs::remove_dir_all(dir).unwrap();
     }
 
     #[test]
     fn does_not_include_directories() {
-        let result = walk_dir(Path::new(".")).unwrap();
+        let dir = create_temp_dir("not-include-dir");
+
+        std::fs::write(dir.join("j.txt"), "").unwrap();
+        std::fs::write(dir.join("k.txt"), "").unwrap();
+        std::fs::create_dir_all(dir.join("test")).unwrap();
+        std::fs::write(dir.join("test/l.txt"), "").unwrap();
+
+        let result = walk_dir(&dir).unwrap();
         for path in &result {
             let p = Path::new(path);
             assert!(p.is_file(), "non-file in result: {:?}", p);
         }
+
+        std::fs::remove_dir_all(dir).unwrap();
+    }
+
+    #[test]
+    fn walks_current_dir_not_include_git() {
+        let dir = create_temp_dir("not-include-git");
+
+        std::fs::write(dir.join("a.txt"), "").unwrap();
+        std::fs::write(dir.join("b.txt"), "").unwrap();
+        std::fs::create_dir_all(dir.join(".git")).unwrap();
+        std::fs::write(dir.join(".git/l.txt"), "").unwrap();
+
+        let result = walk_dir(&dir).unwrap();
+        assert!(result.iter().any(|p| p.ends_with("a.txt")));
+        assert!(result.iter().any(|p| p.ends_with("b.txt")));
+        assert!(!result.iter().any(|p| p.ends_with(".git/l.txt")));
+
+        std::fs::remove_dir_all(dir).unwrap();
     }
 }
